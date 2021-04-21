9de0eb48578b67d5b093382077c6a00e887ef4d3
Henry Craddock <hcraddoc@testium.cs.uoregon.edu>
2021-04-21 11:18:53 -0700

    Updated app.py to handle errors

--- web/app.py
+++ web/app.py
@@ -10,5 +10,16 @@ app = Flask(__name__)
 def hello():
     return "UOCIS docker demo!\n"
 
+
+@app.errorhandler(404)
+def not_found(e):
+    return send_from_directory('templates', '404.html'), 404
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    return send_from_directory('templates', '403.html'), 403
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
